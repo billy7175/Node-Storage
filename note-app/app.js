95be5eb36b3d89b7541145e8d905f65e383a255a
billy7175 <68532934+billy7175@users.noreply.github.com>
2022-02-25 22:33:12 +0900

    NPM Validator isEmail, isURL

--- note-app/app.js
+++ note-app/app.js
@@ -4,6 +4,13 @@ const sum = add(4,2);
 const getNotes = require('./notes');
 const msg = getNotes();
 
+const validator = require('validator')
+
 console.log(`my name is ${firstName}`);
 console.log(`sum is ${sum}`);
 console.log(`msg is ${msg}`)
+
+console.log(validator);
+console.log(validator.isEmail('example@gmail.com'));
+console.log(validator.isURL('https://mead.io'))
+console.log(validator.isURL('https:/mead.io'))
