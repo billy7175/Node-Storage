69b10093fcad4b271f01fc3ff940bd5cd213f3b7
billy7175 <68532934+billy7175@users.noreply.github.com>
2022-02-26 21:03:18 +0900

    NPM install Chalk

--- note-app/app.js
+++ note-app/app.js
@@ -3,6 +3,7 @@ const add = require('./fn.js');
 const sum = add(4,2);
 const getNotes = require('./notes');
 const msg = getNotes();
+const chalk = require('chalk');
 
 const validator = require('validator')
 
@@ -14,3 +15,9 @@ console.log(validator);
 console.log(validator.isEmail('example@gmail.com'));
 console.log(validator.isURL('https://mead.io'))
 console.log(validator.isURL('https:/mead.io'))
+
+const greenMsg = chalk.green('Success!');
+console.log('@greenMsg :' , greenMsg);
+console.log(chalk.green('Success!'))
+console.log(chalk.green.bold('Success!'))
+console.log(chalk.green.bold.inverse('Success!'))
