8a02c8414d0b6c1c4ca33966d89ba1044bddee68
billy7175 <68532934+billy7175@users.noreply.github.com>
2022-02-27 19:08:20 +0900

    Getting input from user

--- note-app/app.js
+++ note-app/app.js
@@ -1,23 +1,10 @@
-const firstName = require('./utils.js');
-const add = require('./fn.js');
-const sum = add(4,2);
-const getNotes = require('./notes');
-const msg = getNotes();
-const chalk = require('chalk');
+const command = process.argv[2];
 
-const validator = require('validator')
 
-console.log(`my name is ${firstName}`);
-console.log(`sum is ${sum}`);
-console.log(`msg is ${msg}`)
-
-console.log(validator);
-console.log(validator.isEmail('example@gmail.com'));
-console.log(validator.isURL('https://mead.io'))
-console.log(validator.isURL('https:/mead.io'))
-
-const greenMsg = chalk.green('Success!');
-console.log('@greenMsg :' , greenMsg);
-console.log(chalk.green('Success!'))
-console.log(chalk.green.bold('Success!'))
-console.log(chalk.green.bold.inverse('Success!'))
+if(command ==='add'){
+  console.log('Adding note.');
+} else if(command ==='remove'){
+  console.log('Removing note!');
+} else {
+  console.log('Type correct command.');
+}
---
+++ note-app/pratice.js
@@ -0,0 +1,26 @@
+const firstName = require('./utils.js');
+const add = require('./fn.js');
+const sum = add(4,2);
+const getNotes = require('./notes');
+const msg = getNotes();
+const chalk = require('chalk');
+
+const validator = require('validator')
+
+console.log(`my name is ${firstName}`);
+console.log(`sum is ${sum}`);
+console.log(`msg is ${msg}`)
+
+console.log(validator);
+console.log(validator.isEmail('example@gmail.com'));
+console.log(validator.isURL('https://mead.io'))
+console.log(validator.isURL('https:/mead.io'))
+
+const greenMsg = chalk.green('Success!');
+console.log('@greenMsg :' , greenMsg);
+console.log(chalk.green('Success!'))
+console.log(chalk.green.bold('Success!'))
+console.log(chalk.green.bold.inverse('Success!'))
+
+console.log(process.argv)
+console.log(process.argv[2])
