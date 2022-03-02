b822a39520b571447fcb853c7b7bda96cc1bef3e
조현 <hcho@growthsoft.co.kr>
2022-03-02 23:13:04 +0900

    Storing data with Json

---
+++ playground/1-json.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+// const book = {
+//     title: 'Ego is the Enemy',
+//     author: 'Ryan Holiday'
+// }
+
+// const bookJSON = JSON.stringify(book);
+// fs.writeFileSync('1-json.json', bookJSON);
+// console.log(bookJSON);
+
+// const parseData = JSON.parse(bookJSON);
+// console.log(parseData.author);
+
+const dataBuffer = fs.readFileSync('1-json.json');
+const dataJSON = dataBuffer.toString();
+const user =JSON.parse(dataJSON);
+user.name ='Gunther';
+user.age = 54;
+
+console.log(dataBuffer);
+console.log(dataJSON);
+const userJSON = JSON.stringify(user);
+fs.writeFileSync('1-json.json', userJSON);
