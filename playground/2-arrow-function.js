6a145d702a95ab06035ae0e3176f99e9fe528af2
LAPTOP-C71E3L3P\Billy <whgus439@gmail.com>
2022-03-08 23:47:53 +0900

    Arrow Function knowledge

--- playground/2-arrow-function.js
+++ playground/2-arrow-function.js
@@ -14,13 +14,28 @@ console.log(fn03(3));
 
 const eventObj = {
   name:'Birthday Party',
+  guestList:['Harry','John','Bill'],
   printGuestList01 : function(){
     console.log("Guest list for " + this.name)
   },
   printGuestList02 : () => {
     console.log("Guest list for " + this.name)
+  },
+  printGuestList03 (){
+    const that = this
+    this.guestList.forEach(function(user){
+      console.log(user + ' is attending ' + that.name, this.name);
+    })
+  },
+  printGuestList04 (){
+    // const that = this
+    this.guestList.forEach((user) => {
+      console.log(user + ' is attending ' + this.name);
+    })
   }
 }
 
 eventObj.printGuestList01();
 eventObj.printGuestList02();
+eventObj.printGuestList03();
+eventObj.printGuestList04();
