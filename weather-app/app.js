78f96a4bcf73b5b878e2cb717dccffe00ec18394
LAPTOP-C71E3L3P\Billy <whgus439@gmail.com>
2022-03-15 01:25:55 +0900

    Handling Error

--- weather-app/app.js
+++ weather-app/app.js
@@ -11,3 +11,17 @@ const url = 'http://api.weatherstack.com/current?access_key=c856b41f18ba9d50e97c
 request({ url: url, json: true }, (error, response) => {
     console.log(response.body.current.weather_descriptions[0] + ". It is currently " + response.body.current.temperature + " degress out.")
 })
+
+const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYmlsbHk3MTc1IiwiYSI6ImNsMHF0anBlaTJkNDQzcm9hMmVvYzA1YTQifQ.OynpLvJpjEYoK7PmKcGYtQ'
+
+request({ url: geocodeURL, json: true }, (error, response) => {
+    if (error) {
+        console.log('Unable to connect to location services!')
+    } else if (response.body.features.length === 0) {
+        console.log('Unable to find location. Try another search.')
+    } else {
+        const latitude = response.body.features[0].center[0]
+        const longitude = response.body.features[0].center[1]
+        console.log(latitude, longitude)
+    }
+})
