621f7983de7488ce205e2393cd4c99d5188b43de
LAPTOP-C71E3L3P\Billy <whgus439@gmail.com>
2022-03-18 01:59:01 +0900

    Advanced Templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,19 +1,27 @@
-const path = require('path')
-const express = require('express');
-
+const path = require("path");
+const express = require("express");
+const hbs = require("hbs");
 
 const app = express();
-const publicDirectoryPath = path.join(__dirname, '../public')
-const viewPath = path.join(__dirname, '../templates')
 
-app.set('view engine', 'hbs');
-app.set('views', viewPath)
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+
+app.set("view engine", "hbs");
+app.set("views", viewPath);
+hbs.registerPartials(partialsPath);
+
 app.use(express.static(publicDirectoryPath));
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   // res.send('<h1>Server is running.</h1>');
-  res.render('index')
-})
+  res.render("index", {
+    helpText: "This is some helpful text.",
+    title: "Help",
+    name: "Andrew",
+  });
+});
 
 // app.get('/help', (req, res) => {
 //   res.send({
@@ -22,23 +30,33 @@ app.get('/', (req, res) => {
 //   });
 // })
 
-// app.get('/about', (req, res) => {
-//   res.send('<h1>about</h1>');
-// })
+app.get("/about", (req, res) => {
+  res.render("about", {
+    helpText: "This is some helpful text.",
+    title: "Help",
+    name: "Mead",
+  });
+});
 
-app.get('/weather', (req, res) => {
+app.get("/help", (req, res) => {
+  res.render("help", {
+    helpText: "This is some helpful text.",
+    title: "Help",
+    name: "Andrew Mead",
+  });
+});
+
+app.get("/weather", (req, res) => {
   res.send({
     forecase: "It is snowing.",
-    location: 'Philadelphia'
+    location: "Philadelphia",
   });
-})
-
-
+});
 
 const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on ${PORT}`)
-})
+  console.log(`Server is running on ${PORT}`);
+});
 
 // console.log(express);
 // console.log(app);
