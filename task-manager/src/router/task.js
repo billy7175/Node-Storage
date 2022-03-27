4bb0e16e8dfa4be94ecb837ef44f20c7e4212484
LAPTOP-C71E3L3P\Billy <whgus439@gmail.com>
2022-03-28 00:48:52 +0900

    Autheticating Task Endpoints

--- task-manager/src/router/task.js
+++ task-manager/src/router/task.js
@@ -4,10 +4,12 @@ const auth = require('../middleware/auth')
 const router = new express.Router();
 
 
-router.get("/tasks", async (req, res) => {
+router.get("/tasks",auth , async (req, res) => {
   try {
-    const tasks = await Task.find({});
-    res.send(tasks);
+    // const tasks = await Task.find({});
+    // res.send(tasks);
+    await req.user.populate('tasks');
+    res.send(req.user.tasks)
   } catch (e) {
     res.status(500).send();
   }
@@ -40,7 +42,7 @@ router.post("/tasks",auth, async (req, res) => {
 });
 
 
-router.patch('/tasks/:id', async (req, res) => {
+router.patch('/tasks/:id',auth, async (req, res) => {
   const updates = Object.keys(req.body)
   const allowedUpdates = ['description', 'completed']
   const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
@@ -50,24 +52,25 @@ router.patch('/tasks/:id', async (req, res) => {
   }
 
   try {
-      const task = await Task.findById(req.params.id);
-      updates.forEach(update => task[update] = req.body[update])
-      await task.save();
+    const task = await Task.findOne({ _id: req.params.id, owner: req.user._id})
+      
       // const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true})
 
       if (!task) {
           return res.status(404).send("not found")
       }
-
+      updates.forEach(update => task[update] = req.body[update])
+      await task.save();
       res.send(task)
   } catch (e) {
       res.status(400).send(e)
   }
 })
 
-router.delete('/tasks/:id', async (req, res) => {
+router.delete('/tasks/:id',auth, async (req, res) => {
   try {
-      const task = await Task.findByIdAndDelete(req.params.id)
+      // const task = await Task.findByIdAndDelete(req.params.id)
+      const task = await Task.findOneAndDelete({ _id: req.params.id, owner: req.user._id })
 
       if (!task) {
           res.status(404).send()
