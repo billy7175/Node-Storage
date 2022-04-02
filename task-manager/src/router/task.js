ff13ad24f3fbadf0ea269f73e4fbd15ddd3f76fa
billy7175 <68532934+billy7175@users.noreply.github.com>
2022-04-02 16:40:47 +0900

    Task Data Pagination

--- task-manager/src/router/task.js
+++ task-manager/src/router/task.js
@@ -1,23 +1,26 @@
-const express = require('express');
-const Task = require('../models/task')
-const auth = require('../middleware/auth')
+const express = require("express");
+const Task = require("../models/task");
+const auth = require("../middleware/auth");
 const router = new express.Router();
 
+router.get("/tasks", auth, async (req, res) => {
+  const match = {};
 
-router.get("/tasks",auth , async (req, res) => {
-  const match = {}
-
-  if(req.query.completed) {
-    match.completed = req.query.completed === 'true'
+  if (req.query.completed) {
+    match.completed = req.query.completed === "true";
   }
   try {
     // const tasks = await Task.find({});
     // res.send(tasks);
     await req.user.populate({
-      path:'task',
-      match
+      path: "task",
+      match,
+      options: {
+        limit: parseInt(req.query.limit),
+        skip: parseInt(req.query.skip),
+      },
     });
-    res.send(req.user.tasks)
+    res.send(req.user.tasks);
   } catch (e) {
     res.status(500).send();
   }
@@ -33,62 +36,67 @@ router.get("/tasks/:id", async (req, res) => {
   }
 });
 
-
-router.post("/tasks",auth, async (req, res) => {
+router.post("/tasks", auth, async (req, res) => {
   // const task = new Task(req.body);
   const task = new Task({
     ...req.body,
-    owner: req.user._id
-})
+    owner: req.user._id,
+  });
 
-  try{
-      await task.save();
-      res.status(201).send(task);
-  }catch(e){
-      res.status(400).send();
+  try {
+    await task.save();
+    res.status(201).send(task);
+  } catch (e) {
+    res.status(400).send();
   }
 });
 
-
-router.patch('/tasks/:id',auth, async (req, res) => {
-  const updates = Object.keys(req.body)
-  const allowedUpdates = ['description', 'completed']
-  const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+router.patch("/tasks/:id", auth, async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["description", "completed"];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
 
   if (!isValidOperation) {
-      return res.status(400).send({ error: 'Invalid updates!' })
+    return res.status(400).send({ error: "Invalid updates!" });
   }
 
   try {
-    const task = await Task.findOne({ _id: req.params.id, owner: req.user._id})
-      
-      // const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true})
+    const task = await Task.findOne({
+      _id: req.params.id,
+      owner: req.user._id,
+    });
 
-      if (!task) {
-          return res.status(404).send("not found")
-      }
-      updates.forEach(update => task[update] = req.body[update])
-      await task.save();
-      res.send(task)
+    // const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true})
+
+    if (!task) {
+      return res.status(404).send("not found");
+    }
+    updates.forEach((update) => (task[update] = req.body[update]));
+    await task.save();
+    res.send(task);
   } catch (e) {
-      res.status(400).send(e)
+    res.status(400).send(e);
   }
-})
+});
 
-router.delete('/tasks/:id',auth, async (req, res) => {
+router.delete("/tasks/:id", auth, async (req, res) => {
   try {
-      // const task = await Task.findByIdAndDelete(req.params.id)
-      const task = await Task.findOneAndDelete({ _id: req.params.id, owner: req.user._id })
+    // const task = await Task.findByIdAndDelete(req.params.id)
+    const task = await Task.findOneAndDelete({
+      _id: req.params.id,
+      owner: req.user._id,
+    });
 
-      if (!task) {
-          res.status(404).send()
-      }
+    if (!task) {
+      res.status(404).send();
+    }
 
-      res.send(task)
+    res.send(task);
   } catch (e) {
-      res.status(500).send()
+    res.status(500).send();
   }
-})
+});
 
-
-module.exports = router
+module.exports = router;
