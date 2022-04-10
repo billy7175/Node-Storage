3685f910be2d11062ddc43970e21d2756c1093bb
billy7175 <whgus439@gmail.com>
2022-04-10 22:13:44 +0900

    Manipulate Form in a different way (e.target.elements.message.value)

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -4,20 +4,10 @@ socket.on('message', (message) => {
   console.log(message);
 })
 
-const messageForm = document.querySelector('#message-form')
-const messageInput = document.querySelector('input')
-messageForm.addEventListener('submit', (e) => {
+// const messageInput = document.querySelector('input')
+document.querySelector('#message-form').addEventListener('submit', (e) => {
+  
   e.preventDefault();
-  const message = messageInput.value
+  const message = e.target.message.value
   socket.emit('sendMessage', message)
 })
-
-// socket.on('countUpdated', (count) => {
-//   console.log('The count has been updaasdasdted.', count)
-// })
-
-
-// document.querySelector('#increment').addEventListener('click', () => {
-//   console.log('Clicked')
-//   socket.emit('increment')
-// })
