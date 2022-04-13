878d98e079b5380d0f9a3cc6ad312691984915fc
LAPTOP-C71E3L3P\Billy <whgus439@gmail.com>
2022-04-14 02:09:32 +0900

    assign variables and setAttribute && removeAttribute for disabled

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -1,13 +1,25 @@
 const socket = io();
 
+//Element
+const $messageForm = document.querySelector("#message-form")
+const $messageFormInput = document.querySelector('input')
+const $messageFormButton = document.querySelector('button')
+
+
 socket.on("message", (message) => {
   console.log(message);
 });
 
-document.querySelector("#message-form").addEventListener("submit", (e) => {
+$messageForm.addEventListener("submit", (e) => {
   e.preventDefault();
+
+  $messageFormButton.setAttribute('disabled', true)
+
   const message = e.target.message.value;
   socket.emit("sendMessage", message, (error) => {
+    $messageFormButton.removeAttribute('disabled')
+    $messageFormInput.value = ''
+    $messageFormInput.focus();
     if (error) {
       return console.log("error", error);
     }
