c930e4c664f1fe10f016d9ce08b68b7411471a81
LAPTOP-C71E3L3P\Billy <whgus439@gmail.com>
2022-04-13 22:54:49 +0900

    Add bad-words filter package && set callback messages

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -1,28 +1,35 @@
 const socket = io();
 
-socket.on('message', (message) => {
+socket.on("message", (message) => {
   console.log(message);
-})
+});
 
-document.querySelector('#message-form').addEventListener('submit', (e) => {
-  
+document.querySelector("#message-form").addEventListener("submit", (e) => {
   e.preventDefault();
-  const message = e.target.message.value
-  socket.emit('sendMessage', message, () => {
-    console.log('Message has been delivered.')
-  })
-})
+  const message = e.target.message.value;
+  socket.emit("sendMessage", message, (error) => {
+    if (error) {
+      return console.log("error", error);
+    }
+    console.log("Message has been delivered.");
+  });
+});
 
-
-document.querySelector('#send-location').addEventListener('click', () => {
+document.querySelector("#send-location").addEventListener("click", () => {
   if (!navigator.geolocation) {
-      return alert('Geolocation is not supported by your browser.')
+    return alert("Geolocation is not supported by your browser.");
   }
 
   navigator.geolocation.getCurrentPosition((position) => {
-      socket.emit('sendLocation', {
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude
-      })
-  })
-})
+    socket.emit(
+      "sendLocation",
+      {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      },
+      () => {
+        console.log("Location has been shared with other people.");
+      }
+    );
+  });
+});
