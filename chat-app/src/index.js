3221b6299431d34381a8f3e6ad66a22e2c29af26
LAPTOP-C71E3L3P\Billy <whgus439@gmail.com>
2022-04-18 01:22:29 +0900

    Add username && admin when sending messages

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -27,8 +27,8 @@ io.on('connection', (socket) => {
         }
         socket.join(user.room)
 
-        socket.emit('message', generateMessage('Welcome!'))
-        socket.broadcast.to(user.room).emit('message', generateMessage(`${user.username} has joined!`))
+        socket.emit('message', generateMessage('Admin', 'Welcome!'))
+        socket.broadcast.to(user.room).emit('Admin', 'message', generateMessage(`${user.username} has joined!`))
 
         callback();
         // socket.emit, io.emit, socket.broadcast.emit
@@ -37,11 +37,12 @@ io.on('connection', (socket) => {
 
 
     socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id)
         const filter = new Filter
         if(filter.isProfane(message)){
             return callback('Profanity is not allowed.')
         }
-        io.to(user.room).emit('message', generateMessage(message))
+        io.to(user.room).emit('message', generateMessage(user.username, message))
         callback();
     })
 
@@ -55,7 +56,7 @@ io.on('connection', (socket) => {
         const user = removeUser(socket.id)
 
         if(user){
-            io.to(user.room).emit('message',generateMessage(`${user.username} has left!`))
+            io.to(user.room).emit('Admin', 'message',generateMessage(`${user.username} has left!`))
         }
 
     })
